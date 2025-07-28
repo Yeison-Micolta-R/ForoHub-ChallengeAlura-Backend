4c7dcdc0bcf74dee23e0168b0d4e746ae5f04ea7
Yeison Micolta <163685952+Yeison-Micolta-R@users.noreply.github.com>
2025-07-27 22:44:08 -0500

    Agregando dependencias

---
+++ src/main/java/com/challenge/aluraforohub/AluraforohubApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.aluraforohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraforohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraforohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/aluraforohub/AluraforohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.aluraforohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraforohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
